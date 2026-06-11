7a54772cf92a9d310809757f596176f24b11536e
jyotsnakushwaha04121997 <jyotsnakushwaha04121997@gmail.com>
2026-06-11 10:00:19 +0530

    connected Db and created student entity to connect with DB table student

--- src/main/java/com/practise/learningRestApis/entity/StudentEntity.java
+++ src/main/java/com/practise/learningRestApis/entity/StudentEntity.java
@@ -1,5 +1,20 @@
 package com.practise.learningRestApis.entity;
 
-public class StudentEntity {
+import org.springframework.boot.autoconfigure.web.WebProperties.Resources.Chain.Strategy;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Student")
+
+public class StudentEntity {
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+private String name;
+private String email;
 }
